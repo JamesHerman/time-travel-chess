95666249e989515eb078c568a8151842c430c015
JamesHerman <mr.herman.j@gmail.com>
2020-09-19 16:33:14 -0400

    took off helmet

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const app = express();
-const helmet = require('helmet');
-app.use(helmet())
 const server = require('http').createServer(app)
 const io=require('socket.io')(server);
 const path = require('path');
