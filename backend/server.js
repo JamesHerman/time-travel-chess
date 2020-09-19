1d3c7fced85a717a1245dde15daf0255e0f31078
JamesHerman <mr.herman.j@gmail.com>
2020-09-19 16:28:51 -0400

    putting on a helmet

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const helmet = require('helmet');
+app.use(helmet())
 const server = require('http').createServer(app)
 const io=require('socket.io')(server);
 const path = require('path');
